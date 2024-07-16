4b338aef28bb94870b780752b08c5df323712224
Harsh Gupta <guptaharsh728@gmail.com>
2024-07-16 16:53:57 +0530

    food del

---
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import './FoodDisplay.css'
+import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem';
+
+function FoodDisplay({ category }) {
+
+    const {food_list} = useContext(StoreContext);
+
+  return (
+    <div className='food-display' id='food-display'>
+        <h2>Top dishes near you</h2>
+        <div className="food-display-list">
+            {food_list.map((item, index)=>{
+              if(category==="All" || category===item.category){
+                return <FoodItem item={item} index={index}/>
+              }
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default FoodDisplay
